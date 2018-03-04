f179eeb8bb06a5a50933b9ff3088c8979ac08b85
greyishmoon <chris@blythe.plus.com>
2018-03-04 20:46:17 +0000

    Completed basic

--- www/js/index.js
+++ www/js/index.js
@@ -1,26 +1,37 @@
-//----------- CORDOVA ONLOAD ---------------- //
-// Cordova device event listener - will not work in browser
-// document.addEventListener("deviceready", initialise, false);
-
-//----------- HTML ONLOAD ---------------- //
-// JQuery ready event listener
-// $(document).ready(initialise);         // or... $(window).on( "load", readyFn );
+var feedURL = "https://www.metaweather.com/api/location/44418/";
 
 //----------- JQM ONLOAD ---------------- //
 // JQuery Mobile pageloaded event listener
 $(document).on('pageshow', '#pageone', initialise);
 
 function initialise() {
-    // EVENT LISTENERS HERE
+    
+    console.log("loaded");
+    
+    <!-- Use an HTML GET request to obtain data from an API  -->
+    var xmlhttp=new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            
+            <!-- parse the resulting JSON into Javascript Data Object -->
+            <!-- you can use a live parser to inspect the contents of the JSON
+            <!-- http://json.parser.online.fr/ -->
+            var weather= JSON.parse(xmlhttp.responseText);
 
-    // DEMO
-    //set up click event handling for button
-    $('#linkedButton').on("click", updateText);
-    $("#linkedText").text("Text replaced by initialize");
 
-    console.log("initialised");
-}
+            <!-- Define Ractive binding -->
+            var ractive = new Ractive({
+                el: 'container', <!-- where -->
+                template: '#myTemplate', <!-- how -->
+                data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
+            });
 
-function updateText() {
-    $("#linkedText").text("Text NOW replaced by BUTTON");
+        }
+    };
+
+    
+    xmlhttp.open("GET", feedURL, true);
+    xmlhttp.send();
+    
 }
